6e91a7e30a172138ee5a14f9b1a62edbd4794430
danny-mak1993 <danny.mak2@gmail.com>
2023-10-29 23:21:41 -0500

    added applauncher java class

--- src/WeatheringGui.java
+++ src/WeatheringGui.java
@@ -2,6 +2,30 @@ import javax.swing.*;
 
 public class WeatheringGui extends JFrame {
     public WeatheringGui(){
+        // setup our gui and add a title
+        super("Weather App");
 
+        // configure gui to end the program's process once it has been closed
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        // set the size of our gui (in pixels)
+        setSize(450, 650);
+
+        // load our gui at the center of the screen
+        setLocationRelativeTo(null);
+
+        // make our layout manager null to manually position our components within the gui
+        setLayout(null);
+
+        // prevent any resize of our gui
+        setResizable(false);
     }
 }
+
+
+
+
+
+
+
+
