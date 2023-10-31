572069848cab46a31ed1de97a2612176ff64f05f
danny-mak1993 <danny.mak2@gmail.com>
2023-10-30 22:59:36 -0500

    added location search box

--- src/WeatheringGui.java
+++ src/WeatheringGui.java
@@ -1,4 +1,11 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class WeatheringGui extends JFrame {
     public WeatheringGui(){
@@ -19,8 +26,25 @@ public class WeatheringGui extends JFrame {
 
         // prevent any resize of our gui
         setResizable(false);
+
+        addGuiComponents();
+    }
+
+    private void addGuiComponents() {
+        // search field
+        JTextField searchTextField = new JTextField();
+
+        // set the location and size of our component
+        searchTextField.setBounds(15, 15, 351, 45);
+
+        // change the font style and size
+        searchTextField.setFont(new Font("Dialog", Font.PLAIN, 24));
+
+        add(searchTextField);
     }
 }
+
+
 
 
 
