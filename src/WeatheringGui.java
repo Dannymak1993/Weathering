7f234b2f2e1a3871805afffbe9be725b0a7c0455
danny-mak1993 <danny.mak2@gmail.com>
2023-11-04 10:55:11 -0500

    default text changed

--- src/WeatheringGui.java
+++ src/WeatheringGui.java
@@ -5,6 +5,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -35,8 +37,13 @@ public class WeatheringGui extends JFrame {
     }
 
     private void addGuiComponents() {
+        // Add an empty JLabel to receive the initial focus
+        JLabel emptyLabel = new JLabel();
+        emptyLabel.setBounds(0, 0, 1, 1);
+        add(emptyLabel);
+
         // search field
-        JTextField searchTextField = new JTextField();
+        JTextField searchTextField = new JTextField("Enter city name");
 
         // set the location and size of our component
         searchTextField.setBounds(15, 15, 351, 45);
@@ -84,7 +91,7 @@ public class WeatheringGui extends JFrame {
         add(windspeedImage);
 
         // windspeed text
-        JLabel windspeedText = new JLabel("<html><b>Windspeed</b> 15km/h</html>");
+        JLabel windspeedText = new JLabel("<html><b>Windspeed</b> 5mph</html>");
         windspeedText.setBounds(310, 500, 85, 55);
         windspeedText.setFont(new Font("Dialog", Font.PLAIN, 16));
         add(windspeedText);
